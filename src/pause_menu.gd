afe0eac700a00ba020e8372344050dd230a93b2c
Avgust Varlamov <varl.avgust@yandex.ru>
2024-08-27 13:50:49 +0300

    add: UI victory, lose menu template

--- src/pause_menu.gd
+++ src/pause_menu.gd
@@ -1,17 +1,6 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-
 func _on_b_back_to_menu_pressed():
 	visible = false
 	get_tree().paused = false
@@ -19,12 +8,13 @@ func _on_b_back_to_menu_pressed():
 	get_parent().get_node("gm").queue_free()
 
 
-func _on_b_retry_pressed() -> void:
+func _on_b_retry_pressed():
 	visible = false
 	get_tree().paused = false
 	get_parent().get_node("gm").free()
 	get_parent().play()
+	
 
-func _on_b_continue_pressed() -> void:
+func _on_b_continue_pressed():
 	visible = false
 	get_tree().paused = false
